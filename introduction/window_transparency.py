e9bfb7729968e45a805998a80cd7844a17979f77
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-14 18:57:16 +0100

    update

--- introduction/window_transparency.py
+++ introduction/window_transparency.py
@@ -7,8 +7,9 @@ def main(page: ft.Page):
     page.window_frameless = True
     page.window_always_on_top = True
     # to make page transparent
-    # page.views[0].bgcolor = ft.colors.TRANSPARENT
-    print(page.views)
+    # page.window_bgcolor = ft.colors.TRANSPARENT
+    page.bgcolor = ft.colors.TRANSPARENT
+    # print(page.views)
     t = ft.Text("Hi", weight=ft.FontWeight.W_900, size=400)
     page.add(t)
 
